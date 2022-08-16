f2bcc265e88c9e36a07933f8e1bdca4444b41e43
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-08-16 17:27:53 -0500

    types refact

--- src/ts/countEffect.ts
+++ src/ts/countEffect.ts
@@ -1,9 +1,9 @@
 export function countEffect(): void {
-  const counter: HTMLElement = document.querySelector<HTMLElement | any>('.miembros')
+  const counter: HTMLElement = document.querySelector<HTMLElement>('.miembros') as HTMLElement
   let contador = 1000
   const interval = setInterval(() => {
     contador++
-    //counter.textContent = contador.toString()
+    counter.textContent = contador.toString()
     if (contador > 1250) {
       clearInterval(interval)
     }
